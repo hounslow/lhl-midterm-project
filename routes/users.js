cbf3ec37fb3e8101bfbb3661405b2b2adddb6d0e
hounslow <hounslowmatthew@gmail.com>
2017-08-18 01:26:47 +0000

    Created the update_profile and new_resource views

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const router  = express.Router();
+const cookieParser = require('cookie-parser')
+
+router.use(cookieParser());
 
 module.exports = (knex) => {
   router.get("/", (req, res) => {
@@ -12,5 +15,24 @@ module.exports = (knex) => {
         res.json(results);
     });
   });
+
+  router.get("/:id", (req, res) => {
+    const templateVariable = {user_id: req.params.id};
+    res.render("update_profile", templateVariable);
+  });
+
+  router.put("/:id", (req, res) => {
+    knex('users').where('id', req.cookies.user_id).update({
+      name: req.body.inputName3,
+      email: req.body.inputEmail3,
+      password: req.body.inputPassword3
+    })
+  });
+
+  app.post("/login/:id", (req, res) => {
+    res.cookie('user_id', req.params.id);
+    res.redirect('/');
+});
+
   return router;
 }
