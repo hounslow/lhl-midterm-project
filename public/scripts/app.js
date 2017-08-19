94862010efdd656ce82dcc22b61cc37e63b07cd7
hounslow <hounslowmatthew@gmail.com>
2017-08-19 00:33:02 +0000

    Resources are being loaded correctly

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -46,5 +46,31 @@ $("i.fa.fa-plus.fa-2x").click(function() {
   $("form.container").slideToggle("fast");
     $("input#resource_title").focus();
   });
-  // });
+
+  $.ajax({
+    method: "GET",
+    url: "/get/resources"
+  }).done((resources) => {
+    for (resource of resources){
+      $("div.row.justify-content-around").append($(`
+        <article class="col-4 resource-block">
+        <div class="card-header">
+          ${resource.title}
+        </div>
+        <div class="card-text">
+          ${resource.description}
+        </div>
+        <div class="card-footer">
+          <div class="url-like">
+            <span class="url"> ${resource.url}</span>
+            <span class="like"> <i class="fa fa-heart-o" aria-hidden="true"></i> <i class="fa fa-heart" aria-hidden="true"></i> 5 </span>
+          </div>
+          <div class="username-comments">
+            <span class="username">By username_1</span>
+            <span class="comments"> <i class="fa fa-comments" aria-hidden="true"></i> Comments </span>
+          </div>
+        </div>
+        </article>`));
+    }
+  });
 });
